dae58ca223f6a6236e304cf70c00e9c4a6c6ea0a
Kevin Becker <kevinfjbecker@gmail.com>
2025-11-09 14:12:53 +0100

    Not working, but funny

--- src/game.js
+++ src/game.js
@@ -311,7 +311,7 @@ Player.prototype.update = function (time, state, keys) {
     if (keys.ArrowLeft) { xSpeed -= playerXSpeed }
     if (keys.ArrowRight) { xSpeed += playerXSpeed }
     let pos = this.pos
-    let movedX = pos.plus(new Vec(x.Speed * time, 0))
+    let movedX = pos.plus(new Vec(xSpeed * time, 0))
     if (!state.level.touches(movedX, this.size, 'wall')) {
         pos = movedX
     }
@@ -328,6 +328,67 @@ Player.prototype.update = function (time, state, keys) {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-let simpleLevel = new Level(simpleLevelPlan)
-let display = new DOMDisplay(document.body, simpleLevel)
-display.syncState(State.start(simpleLevel))
+function trackKeys(keys) {
+    let down = Object.create(null)
+    function track(event) {
+        if (keys.includes(event.key)) {
+            down[event.key] = event.type == 'keydown'
+            event.preventDefault()
+        }
+    }
+    window.addEventListener('keydown', track)
+    window.addEventListener('keyup', track)
+    return down
+}
+
+const arrowKeys = trackKeys(['ArrowLeft', 'ArrowRight, ArrowUp'])
+
+///////////////////////////////////////////////////////////////////////////////
+
+function runAnimation(frameFunc) {
+    let lastTime = null
+    function frame(time) {
+        if (lastTime != null) {
+            let timeStep = Math.min(time - lastTime, 100) / 1000
+            if (frameFunc(timeStep) === false) { return }
+        }
+        lastTime = time
+        requestAnimationFrame(frame)
+    }
+    requestAnimationFrame(frame)
+}
+
+function runLevel(level, Display) {
+    let display = new Display(document.body, level)
+    let state = State.start(level)
+    let ending = 1
+
+    return new Promise(resolve => {
+        runAnimation(time => {
+            state = state.update(time, arrowKeys)
+            display.syncState(state)
+            if (state.status == 'playing') {
+                return true
+            } else if (ending > 0) {
+                ending -= time
+                return true
+            } else {
+                display.clear()
+                resolve(state.status)
+                return false
+            }
+        })
+    })
+}
+
+async function runGame(plans, Display) {
+    for (let level = 0; level < plans.length;) {
+        let status = await runLevel(new Level(plans[level]), Display)
+        if (status == 'won') { level++ }
+    }
+    console.log('You\'ve won!')
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+runGame([simpleLevelPlan], DOMDisplay)
